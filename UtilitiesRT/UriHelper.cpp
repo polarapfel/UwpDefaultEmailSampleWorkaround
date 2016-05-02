9d2eacf95fc61b87c92c9a06338ed2e8c7a69837
Tobias Weisserth <tobias.weisserth@microsoft.com>
2016-05-02 11:57:36 +0200

    Default Email Sample - workaround with parsing utility

---
+++ UtilitiesRT/UriHelper.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "UriHelper.h"
+
+using namespace UtilitiesRT;
+using namespace Platform;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::Foundation;
+
+String^ UriHelper::GetUriAsString(IProtocolActivatedEventArgs^ args)
+{
+    Uri^ uri = args->Uri;
+    String^ uriAsString = uri->AbsoluteUri;
+    String^ unescapedString = Uri::UnescapeComponent(uriAsString);
+
+    return unescapedString;
+}
---
+++ UtilitiesRT/UriHelper.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+using namespace Platform;
+
+namespace UtilitiesRT
+{
+    public ref class UriHelper sealed
+    {
+    public:
+        /// <summary>
+        /// Get Uri from IProtocolActivatedEventArgs
+        /// </summary>
+        /// <param name="aggregateId">Activation Arguments</param>
+        /// <returns>Uri as string</returns>
+        static String^ GetUriAsString(Windows::ApplicationModel::Activation::IProtocolActivatedEventArgs^ args);
+
+    private:
+        UriHelper();
+    };
+}
